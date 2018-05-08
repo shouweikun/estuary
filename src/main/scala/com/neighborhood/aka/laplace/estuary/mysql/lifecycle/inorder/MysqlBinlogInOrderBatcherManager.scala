c15d261aff26abc6cca910104cfb7f0ca826d8fd
neighborhood <liushouwei@finupgroup.com>
2018-05-08 16:22:09 +0800

    完成mysqlInOrder batcherManager

--- src/main/scala/com/neighborhood/aka/laplace/estuary/mysql/lifecycle/inorder/MysqlBinlogInOrderBatcherManager.scala
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/mysql/lifecycle/inorder/MysqlBinlogInOrderBatcherManager.scala
@@ -4,9 +4,14 @@ import akka.actor.{Actor, ActorLogging, ActorRef}
 import akka.routing.ConsistentHashingRouter.{ConsistentHashMapping, ConsistentHashable}
 import com.alibaba.otter.canal.parse.inbound.mysql.dbsync.TableMetaCache
 import com.alibaba.otter.canal.protocol.CanalEntry
+import com.alibaba.otter.canal.protocol.CanalEntry.{RowChange, RowData}
 import com.neighborhood.aka.laplace.estuary.core.lifecycle
 import com.neighborhood.aka.laplace.estuary.core.lifecycle.{SourceDataBatcher, SyncControllerMessage}
+import com.neighborhood.aka.laplace.estuary.mysql.lifecycle.inorder.MysqlBinlogInOrderBatcherManager.IdClassifier
+import com.neighborhood.aka.laplace.estuary.mysql.source.MysqlConnection
 import com.neighborhood.aka.laplace.estuary.mysql.task.Mysql2KafkaTaskInfoManager
+
+import scala.util.Try
 
 /**
   * Created by john_liu on 2018/5/8.
@@ -17,8 +22,8 @@ class MysqlBinlogInOrderBatcherManager(
                                       ) extends Actor with SourceDataBatcher with ActorLogging {
 
   val syncTaskId = mysql2KafkaTaskInfoManager.syncTaskId
-  val mysqlMetaConnection = mysql2KafkaTaskInfoManager.mysqlConnection.fork
-  var tableMetaCache = buildTableMeta
+  //  val mysqlMetaConnection = mysql2KafkaTaskInfoManager.mysqlConnection.fork
+  //  var tableMetaCache = buildTableMeta
   lazy val ddlHandler = context.child("ddlHandler")
   lazy val router = context.child("router")
 
@@ -34,35 +39,27 @@ class MysqlBinlogInOrderBatcherManager(
 
   def online: Receive = {
     case entry: CanalEntry.Entry => {
-      entry.getHeader.getEventType match {
-        case CanalEntry.EventType.ALTER => {
-          //重新获得tablemataCache
-          tableMetaCache = buildTableMeta
-          ddlHandler.fold {
-            log.error(s"ddlHandler cannot be null ,id:${syncTaskId}");
-            throw new Exception("ddlHandler cannot be null ,id:${syncTaskId}")
-          } {
-            ref => ref ! entry
-          }
-        }
-        //都是dml
-        case _ => {
-          import scala.collection.JavaConverters._
-          val tableName = entry.getHeader.getTableName
-          val dbName = entry.getHeader.getSchemaName
-          val primaryKey = tableMetaCache
-            .getTableMeta(dbName, tableName)
-            .getPrimaryFields
-            .asScala
-            .map(_.getColumnName)
-          entry.getAllFields.keySet().asScala.filter(x=>x.ge)
 
+      import scala.collection.JavaConverters._
+      def parseError = {
+        log.error(s"parse row data error,id:${syncTaskId}");
+        throw new Exception(s"parse row data error,id:${syncTaskId}")
+      }
+
+      val rowChange = Try(CanalEntry.RowChange.parseFrom(entry.getStoreValue)).toOption.getOrElse(parseError)
+      if (rowChange.getIsDdl) ddlHandler.fold(log.error(s"ddlHandler cannot be found,id:$syncTaskId"))(ref => ref ! entry)
+      else {
+        rowChange.getRowDatasList.asScala.foreach {
+          data => router.fold(log.error(s"batcher router cannot be found,id:$syncTaskId"))(ref => ref ! IdClassifier(entry, data))
         }
       }
+
+
     }
   }
 
-  def buildTableMeta: TableMetaCache = {
+  @deprecated
+  def buildTableMeta(mysqlMetaConnection: MysqlConnection): TableMetaCache = {
     val canalMysqlConnection = mysqlMetaConnection.toCanalMysqlConnection
     canalMysqlConnection.connect()
     val tableMetaCache = new TableMetaCache(mysqlMetaConnection.toCanalMysqlConnection)
@@ -87,8 +84,15 @@ class MysqlBinlogInOrderBatcherManager(
 
 object MysqlBinlogInOrderBatcherManager {
 
-  sealed case class IdClassifier(entry: CanalEntry, key: String) extends ConsistentHashable {
-    override def consistentHashKey: Any = key
+  sealed case class IdClassifier(entry: CanalEntry.Entry, rowData: RowData) extends ConsistentHashable {
+
+    import scala.collection.JavaConverters._
+
+    override def consistentHashKey: Any = {
+
+      val key = if (entry.getHeader.getEventType.equals(CanalEntry.EventType.DELETE)) rowData.getBeforeColumnsList.asScala.filter(_.getIsKey).mkString("_") else rowData.getAfterColumnsList.asScala.filter(_.getIsKey).mkString("_")
+      key
+    }
   }
 
 }
