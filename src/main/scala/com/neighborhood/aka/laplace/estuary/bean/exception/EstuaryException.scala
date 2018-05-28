435b302a79e26085124796baa15d4531d43c8f50
neighborhood <liushouwei@finupgroup.com>
2018-05-28 19:48:52 +0800

    重d写excpetion类型

---
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/bean/exception/EstuaryException.scala
@@ -0,0 +1,17 @@
+package com.neighborhood.aka.laplace.estuary.bean.exception
+
+/**
+  * Created by john_liu on 2018/5/28.
+  */
+abstract class EstuaryException(
+                                 message: => String,
+                                 cause: Throwable
+                               )
+  extends Exception(message, cause) {
+
+
+  def this(message: => String) = this(message, null)
+
+  def this(cause: Throwable) = this("", cause)
+}
+
