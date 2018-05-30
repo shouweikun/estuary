63f9e697ef61e6dc8a8dbd725ab56b9898838b42
neighborhood <liushouwei@finupgroup.com>
2018-05-30 14:48:51 +0800

    完成heartbeatListener的改造

--- src/main/scala/com/neighborhood/aka/laplace/estuary/core/lifecycle/prototype/SourceDataSinkerManagerPrototype.scala
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/core/lifecycle/prototype/SourceDataSinkerManagerPrototype.scala
@@ -7,7 +7,7 @@ import com.neighborhood.aka.laplace.estuary.core.task.TaskManager
 /**
   * Created by john_liu on 2018/5/21.
   */
-trait SourceDataSinkerManagerPrototype[Sink <: SinkFunc] extends ActorPrototype with SourceDataSinker {
+trait SourceDataSinkerManagerPrototype extends ActorPrototype with SourceDataSinker {
   /**
     * 任务信息管理器
     */
--- src/main/scala/com/neighborhood/aka/laplace/estuary/core/lifecycle/prototype/SourceDataSinkerPrototype.scala
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/core/lifecycle/prototype/SourceDataSinkerPrototype.scala
@@ -2,7 +2,7 @@ package com.neighborhood.aka.laplace.estuary.core.lifecycle.prototype
 
 import com.neighborhood.aka.laplace.estuary.core.lifecycle.worker.SourceDataSinker
 import com.neighborhood.aka.laplace.estuary.core.sink.SinkFunc
-import com.neighborhood.aka.laplace.estuary.core.task.TaskManager
+import com.neighborhood.aka.laplace.estuary.core.task.{RecourceManager, TaskManager}
 
 /**
   * Created by john_liu on 2018/5/21.
@@ -12,7 +12,14 @@ trait SourceDataSinkerPrototype[Sink <: SinkFunc] extends ActorPrototype with So
     * 任务信息管理器
     */
   val taskManager: TaskManager
-
+  /**
+    * 资源管理器
+    */
+  val resourceManger: RecourceManager[_, _, Sink]
+  /**
+    * sink
+    */
+  lazy val sink = resourceManger.sink
   /**
     * 同步任务id
     */
