e82bbb88b7c3c1337db20a41db99376067169889
neighborhood <john_liu@liushouweideMBP.lan>
2019-01-21 23:30:58 +0800

    add sda Alter operation support

--- src/main/scala/com/neighborhood/aka/laplace/estuary/mysql/lifecycle/reborn/fetch/SdaMysqlBinlogInOrderDirectFetcher.scala
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/mysql/lifecycle/reborn/fetch/SdaMysqlBinlogInOrderDirectFetcher.scala
@@ -1,8 +1,8 @@
 package com.neighborhood.aka.laplace.estuary.mysql.lifecycle.reborn.fetch
 
 import akka.actor.{ActorRef, Props}
-import com.alibaba.otter.canal.parse.inbound.mysql.dbsync.SimpleDdlParser
 import com.alibaba.otter.canal.protocol.CanalEntry
+import com.alibaba.otter.canal.protocol.CanalEntry.EventType
 import com.neighborhood.aka.laplace.estuary.core.sink.mysql.MysqlSinkFunc
 import com.neighborhood.aka.laplace.estuary.mysql.task.Mysql2MysqlTaskInfoManager
 import com.neighborhood.aka.laplace.estuary.mysql.utils.CanalEntryTransUtil
@@ -22,13 +22,20 @@ final class SdaMysqlBinlogInOrderDirectFetcher(
 
   lazy val rule: Map[String, String] = taskManager.tableMappingRule
 
+  private lazy val ALTER_PATTERN = """^\s*[a/A][l/L][t/T][e/E][r/R]\s+[T/t][a/A][b/B][l/L][e/E]\s*(\w+).*""".r
+  A
+
   override protected def executeDdl(entry: CanalEntry.Entry): Unit = {
     val ddlSql = CanalEntryTransUtil.parseStoreValue(entry)(syncTaskId).getSql
-
+    val sdaDbAndTableName: String = rule(s"${entry.getHeader.getSchemaName}.${entry.getHeader.getTableName}")
+    lazy val handleAlter: String = ALTER_PATTERN.replaceFirstIn(ddlSql, sdaDbAndTableName)
     taskManager.wait4TheSameCount() //必须要等到same count
-    val ddlResult = SimpleDdlParser.parse(ddlSql, entry.getHeader.getSchemaName)
-    //todo
-    if (sink.isTerminated) sink.insertSql(ddlSql) //出现异常的话一定要暴露出来
+    //    val ddlResult = SimpleDdlParser.parse(ddlSql, entry.getHeader.getSchemaName)
+    val sdaDdlSql = entry.getHeader.getEventType match {
+      case EventType.ALTER => handleAlter
+      case _ => ???
+    }
+    if (!sink.isTerminated) sink.insertSql(sdaDdlSql) //出现异常的话一定要暴露出来
   }
 }
 
