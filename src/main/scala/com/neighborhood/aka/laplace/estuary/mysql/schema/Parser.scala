9349df996835f37216fa92d03abbf7b57988a392
neighborhood <liushouwei@finupgroup.com>
2019-01-30 20:23:47 +0800

    add Parser

--- src/main/scala/com/neighborhood/aka/laplace/estuary/mysql/schema/Parser.scala
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/mysql/schema/Parser.scala
@@ -1,5 +1,7 @@
 package com.neighborhood.aka.laplace.estuary.mysql.schema
 
+import com.neighborhood.aka.laplace.estuary.bean.exception.schema.InvalidDdlException
+import com.neighborhood.aka.laplace.estuary.core.util.JavaCommonUtil
 import com.neighborhood.aka.laplace.estuary.mysql.schema.defs.ddl.{SchemaChange, TableAlter, TableCreate, TableDrop}
 
 import scala.collection.JavaConverters._
@@ -7,26 +9,80 @@ import scala.collection.JavaConverters._
 /**
   * Created by john_liu on 2019/1/28.
   * 处理Ddl Sql 到 SchemaChange
+  * 将SchemaChange 转换成Ddl Sql
   *
-  * @author neighborhood
+  * @note 传入的DDL只能是单条语句 or @throws InvalidDdlException
+  * @author neighborhood.aka.laplace
   */
 object Parser {
 
-  def parse(ddlSql: String, schemaName: String): List[SchemaChange] = {
+  /**
+    * 解析sql 并进行库表名替换
+    *
+    * @param ddlSql
+    * @param tableMappingRule
+    * @return
+    */
+  def parseAndReplace(ddlSql: String, defaultSchemaName: String, tableMappingRule: SdaSchemaMappingRule): String = {
+
+    parseAndReplaceInternal(ddlSql, defaultSchemaName, tableMappingRule).toDdlSql
+  }
+
+
+  private implicit class SchemaChangeToDdlSqlSyntax(schemaChange: SchemaChange) {
+    def toDdlSql: String = SchemaChangeToDdlSql(schemaChange)
+  }
+
+  def SchemaChangeToDdlSql(schemaChange: SchemaChange): String = ???
+
+  private def handleAlter(tableAlter: TableAlter): String = ???
+
+  private def handleCreate(tableCreate: TableCreate): String = ???
+
+  private def handleDrop(tableDrop: TableDrop): String = ???
+
+  /**
+    * 解析Ddl sql
+    *
+    * @param ddlSql     待解析的ddl sql
+    * @param schemaName 库名称
+    * @return SchemaChange
+    */
+  private def parse(ddlSql: String, schemaName: String): List[SchemaChange] = {
     SchemaChange.parse(schemaName, ddlSql).asScala.toList
   }
 
-  def parseAndReplace(ddlSql: String, targetDbName: String, targetTableName: String): List[SchemaChange] = {
-    val re = parse(ddlSql, targetDbName) //这个实现涉及了对象内部变量的改变
-    re.map {
-      x =>
-        x match {
-          case alter: TableAlter => alter.newDatabase = targetDbName; alter.newTableName = targetTableName;
-          case create: TableCreate => create.database = targetDbName; create.table = targetTableName
-          case drop: TableDrop => drop.database = targetDbName; drop.table = targetTableName
-        }
+  /**
+    * 解析并替换库表名
+    *
+    * @param ddlSql ddlSql
+    * @param defaultSchemaName 默认库名称
+    * @param tableMappingRule
+    * @return
+    */
+  private def parseAndReplaceInternal(ddlSql: String, defaultSchemaName: String, tableMappingRule: SdaSchemaMappingRule): SchemaChange = {
+    val re = parse(ddlSql, defaultSchemaName) //这个实现涉及了对象内部变量的改变
+    if (re.size > 0) throw new InvalidDdlException("only single ddl is supported")
+    re.head match {
+      case alter: TableAlter => {
+        if (JavaCommonUtil.isEmpty(alter.database)) alter.database = alter.newDatabase
+        if (JavaCommonUtil.isEmpty(alter.table)) alter.table = alter.newTableName
+        if (JavaCommonUtil.isEmpty(alter.newDatabase)) alter.newDatabase = alter.database
+        if (JavaCommonUtil.isEmpty(alter.newTableName)) alter.newTableName = alter.table
+        (alter.database, alter.table) = tableMappingRule.getMappingName(alter.database, alter.table)
+        (alter.newDatabase, alter.newTableName) = tableMappingRule.getMappingName(alter.newDatabase, alter.newTableName)
+      }
+      case create: TableCreate => {
+        if (!JavaCommonUtil.isEmpty(create.likeDB) && !JavaCommonUtil.isEmpty(create.likeTable)) (create.likeDB, create.likeTable) = tableMappingRule.getMappingName(create.likeDB, create.likeTable)
+        (create.database, create.table) = tableMappingRule.getMappingName(create.database, create.table)
+      }
+      case drop: TableDrop => {
+        (drop.database, drop.table) = tableMappingRule.getMappingName(drop.database, drop.table)
+      }
     }
-    re
+
+    re.head
   }
+
 
 }
