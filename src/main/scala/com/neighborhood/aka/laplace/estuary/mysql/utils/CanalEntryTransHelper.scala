58fd22f7839aa14c62e7a51fcd621c50d9718f0e
neighborhood <liushouwei@finupgroup.com>
2019-02-25 14:24:36 +0800

    fix bug when value contains \

--- src/main/scala/com/neighborhood/aka/laplace/estuary/mysql/utils/CanalEntryTransHelper.scala
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/mysql/utils/CanalEntryTransHelper.scala
@@ -65,7 +65,7 @@ object CanalEntryTransHelper {
       case x if (x.contains("float")) => value
       case x if (x.contains("double")) => value
       case x if (x.contains("bigint")) => value
-      case _ => s""""${value.replaceAll("\"","""\\"""")}"""" //add escape char handler
+      case _ => s""""${value.replaceAll("\\","""\\""").replaceAll("\"","""\\"""")}"""" //add escape char handler
     }
   }
 
