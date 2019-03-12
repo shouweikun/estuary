4c40b0314c6c41021d9a3945966202b5c5e7a229
neighborhood <liushouwei@finupgroup.com>
2019-03-12 11:56:19 +0800

    add zk

--- src/main/scala/com/neighborhood/aka/laplace/estuary/mongo/util/OplogOffsetHandler.scala
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/mongo/util/OplogOffsetHandler.scala
@@ -1,26 +1,41 @@
 package com.neighborhood.aka.laplace.estuary.mongo.util
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 import com.neighborhood.aka.laplace.estuary.core.source.DataSourceConnection
 import com.neighborhood.aka.laplace.estuary.core.task.PositionHandler
+import com.neighborhood.aka.laplace.estuary.core.util.zookeeper.EstuaryStringZookeeperManager
 import com.neighborhood.aka.laplace.estuary.mongo.source.MongoOffset
 
 /**
   * Created by john_liu on 2019/3/1.
   *
   * mongo的offset记录器
+  *
+  * @note 非线程安全
   * @author neighborhood.aka.laplace
   */
-final class OplogOffsetHandler extends PositionHandler[MongoOffset]{
+final class OplogOffsetHandler(
+                                private val zkManager: EstuaryStringZookeeperManager
+                              ) extends PositionHandler[MongoOffset] {
 
+  private val connectionStatus = new AtomicBoolean()
 
+  override def start(): Unit = {
+    zkManager.start()
+    connectionStatus.set(true)
+  }
 
-  override def start(): Unit = ???
+  override def isStart: Boolean = connectionStatus.get()
 
-  override def isStart: Boolean = ???
+  override def close(): Unit = {
+    zkManager.stop()
+    connectionStatus.set(false)
+  }
 
-  override def close(): Unit = ???
+  override def persistLogPosition(destination: String, logPosition: MongoOffset): Unit = {
 
-  override def persistLogPosition(destination: String, logPosition: MongoOffset): Unit = ???
+  }
 
   override def getlatestIndexBy(destination: String): MongoOffset = ???
 
