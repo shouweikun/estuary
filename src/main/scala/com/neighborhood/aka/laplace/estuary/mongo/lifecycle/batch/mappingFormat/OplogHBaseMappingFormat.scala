025dbe2a5265b0e4ee3995234a1644619350e9fc
neighborhood <liushouwei@finupgroup.com>
2019-03-15 11:20:59 +0800

    add OplogHBaseMappingFormat

---
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/mongo/lifecycle/batch/mappingFormat/OplogHBaseMappingFormat.scala
@@ -0,0 +1,21 @@
+package com.neighborhood.aka.laplace.estuary.mongo.lifecycle.batch.mappingFormat
+
+import com.neighborhood.aka.laplace.estuary.bean.support.HBasePut
+import com.neighborhood.aka.laplace.estuary.mongo.lifecycle
+import com.neighborhood.aka.laplace.estuary.mongo.source.{MongoConnection, MongoOffset}
+
+/**
+  * Created by john_liu on 2019/3/15.
+  */
+class OplogHBaseMappingFormat extends OplogMappingFormat[HBasePut[MongoOffset]] {
+  override def syncTaskId: String = ???
+
+  /**
+    * mongoConnection 用于u事件反查
+    *
+    * @return
+    */
+  override def mongoConnection: MongoConnection = ???
+
+  override def transform(x: lifecycle.OplogClassifier): HBasePut[MongoOffset] = ???
+}
