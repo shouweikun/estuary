e3b2728d5db9a06e0dd9e3fe80e00aa86d31803c
songzhimin <songzhimin@finupgroup.com>
2019-04-12 18:18:43 +0800

    add hdfsMessage bean

---
+++ src/main/scala/com/neighborhood/aka/laplace/estuary/bean/support/HdfsMessage.scala
@@ -0,0 +1,25 @@
+package com.neighborhood.aka.laplace.estuary.bean.support
+
+import com.neighborhood.aka.laplace.estuary.core.offset.ComparableOffset
+import org.apache.hadoop.hbase.client.Put
+
+/**
+  * Created by john_liu on 2019/3/14.
+  *
+  * @author neighborhood.aka.laplace
+  */
+final case class HdfsMessage[A <: ComparableOffset[A]](
+                                                     val dbName:String,
+                                                     val tableName: String,
+                                                     val value: String,
+                                                     val offset: A,
+                                                     val isAbnormal: Boolean = false
+                                                   ) {
+
+  val ts = System.currentTimeMillis()
+
+}
+
+object HdfsMessage {
+  def abnormal[A <: ComparableOffset[A]](dbName:String,tableName:String,offset:A): HdfsMessage[A] = HdfsMessage(dbName,tableName,null,offset,true)
+}
